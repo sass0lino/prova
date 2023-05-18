ec9d37d8cd5808eca7664dba6e548a0831776956
Giovanni Ponso <72951567+sass0lino@users.noreply.github.com>
2023-05-18 18:41:55 +0200

    Update RomanPrinterTest.java

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -15,38 +15,6 @@ public class RomanPrinterTest {
 
 
     @Test
-    public void testCPrinter(){
-        try(MockedStatic<IntegerToRoman> integerToRomanMockedStatic = Mockito.mockStatic(IntegerToRoman.class)){
-            integerToRomanMockedStatic.when(()->IntegerToRoman.convert(100)).thenReturn("C");
-            String letterC =
-                    "  _____  \n"+
-                            " / ____| \n"+
-                            "| |      \n"+
-                            "| |      \n"+
-                            "| |____  \n"+
-                            " \\_____| \n";
-
-            assertEquals(letterC, RomanPrinter.print(100));
-        }
-    }
-
-    @Test
-    public void testDPrinter(){
-        try(MockedStatic<IntegerToRoman> integerToRomanMockedStatic = Mockito.mockStatic(IntegerToRoman.class)){
-            integerToRomanMockedStatic.when(()->IntegerToRoman.convert(500)).thenReturn("D");
-            String letterD =
-                    " _____   \n"+
-                            "|  __ \\  \n"+
-                            "| |  | | \n"+
-                            "| |  | | \n"+
-                            "| |__| | \n"+
-                            "|_____/  \n";
-
-            assertEquals(letterD, RomanPrinter.print(500));
-        }
-    }
-
-    @Test
     public void testMPrinter(){
         try(MockedStatic<IntegerToRoman> integerToRomanMockedStatic = Mockito.mockStatic(IntegerToRoman.class)){
             integerToRomanMockedStatic.when(()->IntegerToRoman.convert(1000)).thenReturn("M");
@@ -61,18 +29,5 @@ public class RomanPrinterTest {
             assertEquals(letterM, RomanPrinter.print(1000));
         }
     }
-    @Test (expected = IllegalArgumentException.class)
-    public void testEmptyRomanNumber(){
-        try(MockedStatic<IntegerToRoman> integerToRomanMockedStatic = Mockito.mockStatic(IntegerToRoman.class)){
-            integerToRomanMockedStatic.when(()->IntegerToRoman.convert(1)).thenReturn("");
-            RomanPrinter.print(1);
-        }
-    }
-    @Test (expected = IllegalArgumentException.class)
-    public void testInvalidRomanNumber(){
-        try(MockedStatic<IntegerToRoman> integerToRomanMockedStatic = Mockito.mockStatic(IntegerToRoman.class)){
-            integerToRomanMockedStatic.when(()->IntegerToRoman.convert(1)).thenReturn("A");
-            RomanPrinter.print(1);
-        }
-    }
+    
 }
